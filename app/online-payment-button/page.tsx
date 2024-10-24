babb6f405e675cc69f1184b7e2830de781e7958d
Bienvenu <faraja.bien@gmail.com>
2024-10-24 16:35:50 +0300

    update service offering

--- app/online-payment-button/page.tsx
+++ app/online-payment-button/page.tsx
@@ -2,9 +2,5 @@ import PayButtonPageClient from '@/components/PayButtonPageClient'
 import React from 'react'
 
 export default function PaymentDemo() {
-	if (typeof window !== 'undefined') {
-		return <PayButtonPageClient />
-	} else {
-		return <div>Loading...</div>
-	}
+	return <PayButtonPageClient />
 }
